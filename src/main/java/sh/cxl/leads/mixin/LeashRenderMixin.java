dd0018b536940e939a2ef8e3efe4677de9521132
Caroline Larimore <caroline@larimo.re>
2025-08-07 15:21:39 -0700

    Only render leash on main hand

--- src/main/java/sh/cxl/leads/mixin/LeashRenderMixin.java
+++ src/main/java/sh/cxl/leads/mixin/LeashRenderMixin.java
@@ -1,10 +1,12 @@
 package sh.cxl.leads.mixin;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.*;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Arm;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,7 +18,7 @@ import sh.cxl.leads.render.LeashRenderer;
 public abstract class LeashRenderMixin {
     @Inject(method = "renderRightArm", at = @At("TAIL"))
     public void renderRightArmLead(MatrixStack matrices, VertexConsumerProvider vertices, int light, AbstractClientPlayerEntity player, CallbackInfo ci) {
-        if (((LeashStateAccess) player).leads$getLeashState()) {
+        if (((LeashStateAccess) player).leads$getLeashState() && MinecraftClient.getInstance().options.getMainArm().getValue() == Arm.RIGHT) {
             PlayerEntityModel<AbstractClientPlayerEntity> model = ((PlayerEntityRenderer) (Object) this).getModel();
             LeashRenderer.renderRightArmLeash(matrices, vertices, light, model);
         }
@@ -24,7 +26,7 @@ public abstract class LeashRenderMixin {
 
     @Inject(method = "renderLeftArm", at = @At("TAIL"))
     public void renderLeftArmLead(MatrixStack matrices, VertexConsumerProvider vertices, int light, AbstractClientPlayerEntity player, CallbackInfo ci) {
-        if (((LeashStateAccess) player).leads$getLeashState()) {
+        if (((LeashStateAccess) player).leads$getLeashState() && MinecraftClient.getInstance().options.getMainArm().getValue() == Arm.LEFT) {
             PlayerEntityModel<AbstractClientPlayerEntity> model = ((PlayerEntityRenderer) (Object) this).getModel();
             LeashRenderer.renderLeftArmLeash(matrices, vertices, light, model);
         }
